34de78d2c437c27358f7ef3ee94cb35930b92eed
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-19 21:20:04 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    sentences = self.split(/[.?!]/).reject {|empty_string| empty_string.empty?}
+    sentences.count
+    
   end
 end
