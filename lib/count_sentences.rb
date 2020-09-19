44278cdd83d19953416ca22a90d4d12b95ed57d9
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-19 21:18:36 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,8 +15,8 @@ class String
   end
 
   def count_sentences
-    sentences = self.split(/[.?!]/).reject {|sentence| sentence.empty?}.size
-    sentences.length
+    sentences = self.split(/[.?!]/).reject {|sentence| sentence.empty?}
+    sentences.count
     
     #binding.pry
   end
